90ec818a5eca52578a37caefeb79db055566dbbb
Abolaji Jamiu <jamiuabolajilanre@gmail.com>
2025-11-09 10:27:03 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -16,16 +16,37 @@ button.addEventListener("click", function () {
   if (!guess) {
     document.querySelector(".message").textContent = "Input a number!";
   } else if (guess === secretNumber) {
-    //to check if the guessed number is equals to the secret number
+    //when player wins game
     document.querySelector(".message").textContent = "Correct Number 🎉";
+
+    //   to change background color when player wins
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    // when players guess is higher
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too high!!!";
-    // to deduct the score by 1 when the guess is higher than the secret number
-    document.querySelector(".score").textContent = score = score - 1;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!!!";
+      // to deduct the score by 1 when the guess is higher than the secret number
+      document.querySelector(".score").textContent = score = score - 1;
+    } else {
+      // to display you lost the game
+      document.querySelector(".message").textContent = "You lost the game!";
+      //   to return score to zero when you loose
+      document.querySelector(".score").textContent = 0;
+    }
+
+    // when players guess is not up to the secret number
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too low!";
-    // to deduct the score by 2 when the guess is lower than the secret number
-    document.querySelector(".score").textContent = score = score - 1;
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      // to deduct the score by 2 when the guess is lower than the secret number
+      document.querySelector(".score").textContent = score = score - 1;
+    } else {
+      // to display you lost the game message
+      document.querySelector(".message").textContent = "You lost the game";
+      //to return score to zero when you loose
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
 
